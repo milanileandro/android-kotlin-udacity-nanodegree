7312ebc785bafda4fa45a36f70018eaa79a1f49e
Milani Leandro <leandromilani@gmail.com>
2020-10-26 17:58:29 -0400

    Add nickname input and update on screen

--- AboutMe/app/src/main/java/com/milanileandro/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/milanileandro/aboutme/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.milanileandro.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener { addNickName(it) }
+    }
+
+    private fun addNickName(view: View) {
+        val nicknameEdit = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameText = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameText.text = nicknameEdit.text
+        nicknameEdit.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
