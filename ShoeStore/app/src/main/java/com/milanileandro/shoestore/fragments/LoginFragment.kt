da77d13573fc7c694ccb9a30af047adb84abf811
Milani Leandro <leandromilani@gmail.com>
2020-12-04 16:12:50 -0400

    Add navigation from login to welcome fragment

--- ShoeStore/app/src/main/java/com/milanileandro/shoestore/fragments/LoginFragment.kt
+++ ShoeStore/app/src/main/java/com/milanileandro/shoestore/fragments/LoginFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.milanileandro.shoestore.R
 import com.milanileandro.shoestore.databinding.FragmentLoginBinding
+import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
     override fun onCreateView(
@@ -22,5 +24,17 @@ class LoginFragment : Fragment() {
         )
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonLogin.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.toWelcomeFragment())
+        }
+
+        buttonCreateAccount.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.toWelcomeFragment())
+        }
     }
 }
