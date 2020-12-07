3c894e46fe48c3ba4171eeafb11673f6720ad812
Milani Leandro <leandromilani@gmail.com>
2020-12-07 11:52:54 -0400

    Refactor Shoe model

--- ShoeStore/app/src/main/java/com/milanileandro/shoestore/model/Shoe.kt
+++ ShoeStore/app/src/main/java/com/milanileandro/shoestore/model/Shoe.kt
@@ -5,6 +5,5 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Shoe(
-    var name: String, var size: Double, var company: String, var description: String,
-    val images: List<String> = mutableListOf()
+    var name: String, var size: Double, var company: String, var description: String
 ) : Parcelable
